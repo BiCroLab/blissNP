3f53992288307660a88f5cf2763b806d9c0e042f
garner1 <silvano.garnerone@gmail.com>
2020-01-23 09:55:14 +0100

    better explanation in prepare_pattern.sh

--- bin/prepare_pattern.sh
+++ bin/prepare_pattern.sh
@@ -6,6 +6,10 @@ while read -r line; do
     f1=`echo $line|cut -d',' -f1`
     f2=`echo $line|cut -d',' -f2|tr -d '_/'`
     f3=`echo $line|cut -d',' -f3`
-    # HERE CHANGE THE NUMBER OF MISMATCHES IN [1,0,0] FROM 1 TO OTHER VALUES, IE [2,0,0] OR [0,0,0]
+    ##############################################################################################
+    # HERE CHANGE THE NUMBER OF MISMATCHES IN [1,0,0] FROM 1 TO OTHER VALUES, IE [2,0,0] OR [0,0,0]:
+    #  echo ^ 8...8 "$f3"[numb_of_mismatches,0,0] 1...1000 $ > "$f1"_"$f2"_"$f3"
+    ###############################################################################################
     echo ^ 8...8 "$f3"[1,0,0] 1...1000 $ > "$f1"_"$f2"_"$f3"
+    ###############################
 done < $inputfile
