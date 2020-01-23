1c4655f96267f65971b011b1f87b539f9db4d878
garner1 <silvano.garnerone@gmail.com>
2020-01-23 12:40:26 +0100

    DSB position on negative strand shifted by -1 on tsv files

--- bin/module/umi_filter_3.sh
+++ bin/module/umi_filter_3.sh
@@ -3,4 +3,7 @@
 input=$1
 output=$2
 
-cat $input | grep -v "_" | sed -e 's/chrX/chr23/g' | sed -e 's/chrY/chr24/g' | cut -f-3 | LC_ALL=C uniq -c | awk '{OFS="\t";print $2,$3,$4,$1}' > $output
+cat $input |
+    awk -F'\t' '{ if ( $4 == "-" ) {$2=$2-1;$3=$3-1;print $0} else {print $0} }'| #correct for the bedtools/bamfile mismatch on negative strands end location 
+    tr ' ' '\t' |
+    grep -v "_" | sed -e 's/chrX/chr23/g' | sed -e 's/chrY/chr24/g' | cut -f-3 | LC_ALL=C uniq -c | awk '{OFS="\t";print $2,$3,$4,$1}' > $output
