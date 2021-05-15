6b2d3fbdadca7a2aa22f9fe46f3d855d65bcb628
fagostini <agostini.federico@gmail.com>
2021-05-15 11:39:16 +0200

    Minor bugfix

--- bin/module/umi_filter_3.sh
+++ bin/module/umi_filter_3.sh
@@ -6,4 +6,4 @@ output=$2
 cat $input |
     awk -F'\t' '{ if ( $4 == "-" ) {$2=$2-1;$3=$3-1;print $0} else {print $0} }'| #correct for the bedtools/bamfile mismatch on negative strands end location 
     tr ' ' '\t' |
-    grep -v "_" | sed -e 's/chrX/chr23/g' | sed -e 's/chrY/chr24/g' | awk '{if($3=="+"){printf "%s\t%d\t%d\n", $1, $2, $3+1}else{printf "%s\t%d\t%d\n", $1, $3, $3+1}}' | sort -k1,1 -k2,2g -k3,3g | LC_ALL=C uniq -c | awk '{OFS="\t";print $2,$3,$4,$1}' > $output
+    grep -v "_" | sed -e 's/chrX/chr23/g' | sed -e 's/chrY/chr24/g' | awk '{if($3=="+"){printf "%s\t%d\t%d\n", $1, $2, $3}else{printf "%s\t%d\t%d\n", $1, $3, $3+1}}' | sort -k1,1 -k2,2g -k3,3g | LC_ALL=C uniq -c | awk '{OFS="\t";print $2,$3,$4,$1}' > $output
